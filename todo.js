8616b22c0b3a51f85a5d742ec6b8d530053abaea
Shreyas <shreyaa1110@gmail.com>
2023-12-06 02:50:29 +0530

    Added renderList

--- todo.js
+++ todo.js
@@ -1,13 +1,48 @@
 const tasks = [];
-const taskList = document.getElementById("list");
+const tasksList = document.getElementById("list");
 const addTaskInput = document.getElementById("add");
 const tasksCounter = document.getElementById("tasks-counter");
 
 console.log("Working");
+function addTaskToDOM(task) {
+  const li = document.createElement("li");
 
-function renderList() {}
+  li.innerHTML = ` 
+  
+    <input type="checkbox" id="${task.id}" data-id="${task.id}" ${
+    task.done ? "checked" : " "
+  } class="custom-checkbox">
+    <label for="${task.id}">${task.text}</label>
+    <img src="https://www.svgrepo.com/show/123434/trash.svg" class="delete" data-id="${task.id}" />
+  
+`;
 
-function markTaskAsComplete(taskId) {}
+  tasksList.append(li);
+}
+function renderList() {
+  tasksList.innerHTML = " ";
+
+  for (let i = 0; i < tasks.length; i++) {
+    addTaskToDOM(tasks[i]);
+  }
+  tasksCounter.innerHTML = tasks.length;
+}
+
+function toggleTask(taskId) {
+  const task = tasks.filter(function (task) {
+    return task.id === taskId;
+  });
+
+  if (task.length > 0) {
+    const currentTask = task[0];
+
+    currentTask.done = !currentTask.done;
+    renderList();
+    showNotification("Task toggled successfully");
+    return;
+  }
+  showNotification("could not toggle the task");
+}
 
 function deleteTask(taskId) {
   const newTasks = tasks.filter(function (task) {
