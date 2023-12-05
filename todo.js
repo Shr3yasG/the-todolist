29ed05fee4bd8c156491fe42f380510caed5f3d5
Shreyas <shreyaa1110@gmail.com>
2023-12-06 01:11:54 +0530

    add and delete task added

--- todo.js
+++ todo.js
@@ -1,40 +1,51 @@
 const tasks = [];
-const taskList = document.getElementById('list');
-const addTaskInput = document.getElementById('add');
-const tasksCounter = document.getElementById('tasks-counter');
+const taskList = document.getElementById("list");
+const addTaskInput = document.getElementById("add");
+const tasksCounter = document.getElementById("tasks-counter");
 
-console.log('Working');
+console.log("Working");
 
-function renderList () {}
+function renderList() {}
 
-function markTaskAsComplete (taskId) {}
+function markTaskAsComplete(taskId) {}
 
-function deleteTask (taskId) {}
+function deleteTask(taskId) {
+  const newTasks = tasks.filter(function (task) {
+    return task.id !== taskId;
+  });
+}
 
-function addTask (task) {}
+function addTask(task) {
+  if (task) {
+    tasks.push(task);
+    renderList();
+    showNotification("Task added sucessfully");
+    return;
+  }
+}
 
 function showNotification(text) {
-    alert(text);
+  alert(text);
 }
 
-function handleInputKeypress (e){
-    if (e.key === 'Enter'){
-        const text = e.target.value;
-        console.log('text', text);
+function handleInputKeypress(e) {
+  if (e.key === "Enter") {
+    const text = e.target.value;
+    console.log("text", text);
 
-        if(!text){
-            showNotification('Task cannot be empty');
-            return;
-        }
-
-        const task = {
-            text,
-            id:Date.now().toString(),
-            done:false
-        }
-
-        e.target.value = '';
-        addTask(task);
+    if (!text) {
+      showNotification("Task cannot be empty");
+      return;
     }
+
+    const task = {
+      text,
+      id: Date.now().toString(),
+      done: false,
+    };
+
+    e.target.value = "";
+    addTask(task);
+  }
 }
-addTaskInput.addEventListener('keyup',handleInputKeypress);
+addTaskInput.addEventListener("keyup", handleInputKeypress);
